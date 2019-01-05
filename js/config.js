85d12f8f14e89f2c8405ae72524eaa8e746803cf
Tim Lohnes <tim@lohneswright.com>
2019-01-04 16:58:11 -0800

    something weird, page not loading rest/services not loading. Arrgghh

---
+++ js/config.js
@@ -0,0 +1,15 @@
+var configFile = {
+  base_URL: "https://maps.hayward-ca.gov/arcgis/rest/services/Ext_Basemaps",
+  defaultLeft: "AerialPhotoHybrid_2017",
+  defaultRight: "AerialPhoto_1968_12inch",
+  //rename: [
+  //  ("Aerial_Imagery_1954", "1954 Aerial"),
+  //  ("AerialPhoto_1968_12inch", "1968 Aerial")
+//  ],
+  exclude: [
+    "Elevation2014",
+    "Slope2014",
+    "UAV_HARDShoreline_KingTide_20180102",
+    "UAV_HARDShoreline_LowTide_20180305"
+  ]
+}
